5b8348e1875242234d9e8b72ce187e8a4a706dd1
Alexa Scott <arscott4792@gmail.com>
2018-03-21 09:40:36 -0700

    added test for get all

--- test/notes.test.js
+++ test/notes.test.js
@@ -3,7 +3,11 @@
 const app = require('../server');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+// const chaiSpies = require('chai-spies');
+const expect = chai.expect;
+
 const mongoose = require('mongoose');
+const express = require('express');
 
 const { TEST_MONGODB_URI } = require('../config');
 
@@ -11,7 +15,8 @@ const Note = require('../models/note');
 
 const seedNotes = require('../db/seed/notes');
 
-const expect = chai.expect;
+chai.use(chaiHttp);
+// chai.use(chaiSpies);
 
 describe('Notes API', function() {
   before(function() {
@@ -30,4 +35,23 @@ describe('Notes API', function() {
   after(function () {
     return mongoose.disconnect();
   });
+
+  describe('GET /api/notes', function() {
+    it('should return the correct number of notes', function() {
+      //1. Call the database and the API
+      const dbPromise = Note.find();
+      const apiPromise = chai.request(app).get('/api/notes');
+      //2. Wait for both promises to resolve using  'Promise.all'
+      return Promise.all([dbPromise, apiPromise])
+      //3. then compare database results to API results
+        .then(([data, res]) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(data.length);
+        });
+    });
+
+  });
+
 });
